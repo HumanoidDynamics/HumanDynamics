26f11ca29480311cf863eff560001bde944a51c2
Uhrendoktor <docclock85@gmail.com>
2019-10-20 01:48:05 +0200

    removed examplemod

--- mod/src/main/java/com/humanoiddynamics/humandynamics/Main.java
+++ mod/src/main/java/com/humanoiddynamics/humandynamics/Main.java
@@ -1,5 +1,8 @@
 package com.humanoiddynamics.humandynamics;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -11,6 +14,7 @@ public class Main {
 	
 	public static Main instance;
 	public final static String MOD_ID = "humandynamics";
+	private static final Logger logger = LogManager.getLogger(MOD_ID);
 	
 	public Main() {
 		instance = this;
