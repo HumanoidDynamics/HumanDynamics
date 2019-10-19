88529b1508efb75c3f127c213eacf14cf4dfa7c1
Uhrendoktor <docclock85@gmail.com>
2019-10-20 01:45:29 +0200

    update

--- mod/src/main/java/com/humanoiddynamics/humandynamics/Main.java
+++ mod/src/main/java/com/humanoiddynamics/humandynamics/Main.java
@@ -1,5 +1,29 @@
 package com.humanoiddynamics.humandynamics;
 
-public class Main {
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
+@Mod("humandynamics")
+public class Main {
+	
+	public static Main instance;
+	public final static String MOD_ID = "humandynamics";
+	
+	public Main() {
+		instance = this;
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientRegistries);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+	
+	private void setup(final FMLCommonSetupEvent event) {
+		
+	}
+	
+	private void clientRegistries(final FMLClientSetupEvent event) {
+		
+	}
 }
