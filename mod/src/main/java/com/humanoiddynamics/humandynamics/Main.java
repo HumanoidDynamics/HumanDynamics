569bc7d3252278eca52ab6b8963b412a6f222234
Uhrendoktor <docclock85@gmail.com>
2019-10-20 02:41:25 +0200

    added first item

--- mod/src/main/java/com/humanoiddynamics/humandynamics/Main.java
+++ mod/src/main/java/com/humanoiddynamics/humandynamics/Main.java
@@ -3,7 +3,14 @@ package com.humanoiddynamics.humandynamics;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.humanoiddynamics.humandynamics.lists.ItemList;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -29,5 +36,15 @@ public class Main {
 	
 	private void clientRegistries(final FMLClientSetupEvent event) {
 		
+	}
+	
+	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+	public static class RegistyEvents {
+		@SubscribeEvent
+		public static void registerItems(final RegistryEvent.Register<Item> event) {
+			event.getRegistry().registerAll(
+				ItemList.for_test_purposes = new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName(new ResourceLocation(MOD_ID, "for_test_purposes"))
+			);
+		}
 	}
 }
---
+++ mod/src/main/java/com/humanoiddynamics/humandynamics/lists/ItemList.java
@@ -0,0 +1,7 @@
+package com.humanoiddynamics.humandynamics.lists;
+
+import net.minecraft.item.Item;
+
+public class ItemList {
+	public static Item for_test_purposes;
+}
